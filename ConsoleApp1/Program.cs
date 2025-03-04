c99b8ed3b8f29f66827884faac12714f73aff7ac
Ramses Sanchez <ramsessanchez25@gmail.com>
2025-03-04 09:53:44 -0500

    Properties added

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,11 @@ class BankAccount
 {
     // Properties of the class to go right here
 
+    public int AccountId{ get ; private set; }
+    public string CustomerName{ get; private set; }
+    public decimal Balance{ get; private set; }
+    public List<string> Transactions { get; private set; }
+
     
 
 
