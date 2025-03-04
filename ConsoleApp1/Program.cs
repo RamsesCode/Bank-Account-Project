697c69a52a01fc33751f591a34eca6313ec043b3
Ramses Sanchez <ramsessanchez25@gmail.com>
2025-03-04 10:06:12 -0500

    constructor added

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 
 class BankAccount
@@ -11,14 +12,27 @@ class BankAccount
     public decimal Balance{ get; private set; }
     public List<string> Transactions { get; private set; }
 
-    
+    // Private 
 
-
-
-
+    private string int _accountCounter = 1000;
 
 
     // initializing the constructor 
+
+    public BankAccount (string customerName, decimal initalBalance)
+    {
+        if (initalBalance < 0)
+    {
+        throw new ArgumentException("Initial Balance is invalid or can not be negative");
+
+    }
+
+    AccountId = ++_accountCounter;
+    CustomerName = customerName;
+    Balance = initalBalance;
+    Transactions = new List<string> ($ "Account created with initial balance: {Balance:C}");
+
+    }
 
 
 
